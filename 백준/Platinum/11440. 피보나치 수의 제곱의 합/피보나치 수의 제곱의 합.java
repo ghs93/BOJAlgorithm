1bb5d191ef80dd7b92cbbc3f6cb9323a37525e32
GAN HOSUNG <94939851+ghs93@users.noreply.github.com>
2023-02-24 13:39:43 +0900

    [Platinum V] Title: 피보나치 수의 제곱의 합, Time: 76 ms, Memory: 11496 KB -BaekjoonHub

---
+++ 백준/Platinum/11440. 피보나치 수의 제곱의 합/피보나치 수의 제곱의 합.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static long[][] matrix = {{1, 1}, {1, 0}};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        long N = Long.parseLong(br.readLine());
+        long[][] mat = pow(N);
+        System.out.println((mat[0][0] * mat[1][0]) % 1000000007);
+    }
+
+    static long[][] pow(long size) {
+        if(size == 0 || size == 1) {
+            return matrix;
+        }
+
+        long[][] d = pow(size / 2);
+        long[][] mat = mul(d, d);
+
+        if(size % 2 != 0) {
+            mat = mul(mat, matrix);
+        }
+
+        return mat;
+    }
+
+    static long[][] mul(long[][] src, long[][] dest) {
+        long[][] mat = new long[2][2];
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                for (int k = 0; k < 2; k++) {
+                    mat[i][j] += src[i][k] * dest[k][j];
+                    mat[i][j] %= 1000000007;
+                }
+            }
+        }
+
+        return mat;
+    }
+}
